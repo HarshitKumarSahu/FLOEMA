54a1a4a685ace9fb6b58ce555aa44d61570182b7
HarshitKumarSahu <poltuharshit14@gmail.com>
2025-06-07 19:32:29 +0530

    added collection.ejs and index.ejs conteent

--- vite.config.js
+++ vite.config.js
@@ -17,3 +17,78 @@ export default defineConfig({
     }
   }
 });
+
+// import { defineConfig } from 'vite';
+// import path from 'path';
+
+// export default defineConfig({
+//   root: 'src',
+//   build: {
+//     outDir: '../public',
+//     rollupOptions: {
+//       input: {
+//         main: path.resolve(__dirname, 'src/js/main.js')
+//       },
+//       output: {
+//         entryFileNames: 'js/[name].js',
+//         chunkFileNames: 'js/[name].js',
+//         assetFileNames: ({ name }) => {
+//           if (/\.css$/.test(name ?? '')) {
+//             return 'css/[name][extname]';
+//           }
+//           return 'assets/[name][extname]';
+//         }
+//       }
+//     }
+//   },
+//   css: {
+//     preprocessorOptions: {
+//       scss: {
+//         additionalData: `@import "./src/scss/variables.scss";`
+//       }
+//     }
+//   }
+// });
+
+
+// import { defineConfig } from 'vite';
+// import path from 'path';
+
+// export default defineConfig({
+//   root: 'src',
+//   build: {
+//     outDir: '../public',
+//     emptyOutDir: true,
+//     rollupOptions: {
+//       input: {
+//         main: path.resolve(__dirname, 'src/js/main.js')
+//       },
+//       output: {
+//         entryFileNames: 'js/[name].js',
+//         chunkFileNames: 'js/[name].js',
+//         assetFileNames: ({ name }) => {
+//           if (/\.css$/.test(name ?? '')) {
+//             return 'css/[name][extname]';
+//           }
+//           return 'assets/[name][extname]';
+//         }
+//       }
+//     }
+//   },
+//   css: {
+//     preprocessorOptions: {
+//       scss: {
+//         additionalData: `@import "./src/scss/variables.scss";`
+//       }
+//     }
+//   },
+//   server: {
+//     port: 5173,
+//     proxy: {
+//       '/': {
+//         target: 'http://localhost:8080',
+//         changeOrigin: true
+//       }
+//     }
+//   }
+// });
